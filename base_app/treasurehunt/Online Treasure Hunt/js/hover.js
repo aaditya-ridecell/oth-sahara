614a6adbd1017c1e671bfe2ca167922948145e86
Yashodhan Agnihotri <work.yashodhaniiitp@gmail.com>
2020-04-24 10:21:02 +0530

    Added Footer, new back and ending page

--- base_app/treasurehunt/Online Treasure Hunt/js/hover.js
+++
@@ -1,8 +0,0 @@
-	$(document).ready(function(){
-		$(".dropdown, .btn-group").hover(function(){
-			var dropdownMenu = $(this).children(".dropdown-menu");
-			if(dropdownMenu.is(":visible")){
-				dropdownMenu.parent().toggleClass("open");
-			}
-		});
-	});     
